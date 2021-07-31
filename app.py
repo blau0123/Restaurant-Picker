40df455a0e9a85bd994d7f455f6b6aa6781e53a9
Brendan <bwlau@ucsd.edu>
2021-07-30 22:31:53 -0700

    Added text boxes. Search given queries works and is displayed

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import requests
 location = ''
 radius = -1
 term = ''
+restaurants = []
 
-def get_restaurants(location, radius, term):
+def get_restaurants():
     headers = {'Authorization': 'Bearer %s' % API_KEY}
     params = {'location': location, 'limit':10}
     if radius != -1:
@@ -16,16 +17,15 @@ def get_restaurants(location, radius, term):
     endpoint = 'https://api.yelp.com/v3/businesses/search'
     resp = requests.get(url=endpoint, headers=headers, params=params)
     data = resp.json()
-    return data['businesses']
+    restaurants = data['businesses']
 
 # Give general information about the program
 def main_page(root):
-    get_restaurants('Tokyo', 10000, '')
     # Create a label widget and pack it onto the window
     title_label = tk.Label(root, text='Restaurant Picker')
     desc_msg = tk.Message(root, text='This executable will randomly choose a restaurant based on a certain radius around you. Choose your location and radius, and optionally choose what type of food you prefer.')
     desc_msg.config(width=400, pady=20)
-    next_btn = tk.Button(root, text='Next', command=page_changer, padx=30, pady=5)
+    next_btn = tk.Button(root, text='Next', command= lambda: page_changer(['']), padx=30, pady=5)
 
     title_label.pack()
     desc_msg.pack()
@@ -36,10 +36,20 @@ def location_sel_page(root):
     title_label = tk.Label(root, text='Choose your location and radius')
     desc_msg = tk.Message(root, text='Your last location chosen is Tokyo. Would you like to change it? If no, leave the input box empty and press next.')
     desc_msg.config(width=400, pady=20)
-    next_btn = tk.Button(root, text='Next', command=page_changer, padx=30, pady=5)
+    input_box = tk.Entry(root, width=50)
+
+    radius_msg = tk.Message(root, text='What radius do you want around your location (in miles)? Max is 25 miles.')
+    radius_msg.config(width=400, pady=20)
+    radius_input_box = tk.Entry(root, width=50)
+
+    next_btn = tk.Button(root, text='Next', command= 
+        lambda: page_changer([input_box.get(), radius_input_box.get()]), padx=30, pady=5)
 
     title_label.pack()
     desc_msg.pack()
+    input_box.pack()
+    radius_msg.pack()
+    radius_input_box.pack()
     next_btn.pack()
 
 # Allow the user to select food choices (Italian, etc.) comma separated
@@ -47,24 +57,36 @@ def choices_page(root):
     title_label = tk.Label(root, text='Food Choices')
     desc_msg = tk.Message(root, text='Choose any specific type of food you want (Italian, sushi, etc.). You can only make one choice. If there is no preference, leave the input box empty and press next.')
     desc_msg.config(width=400, pady=20)
-    next_btn = tk.Button(root, text='Next', command=page_changer, padx=30, pady=5)
+    input_box = tk.Entry(root, width=50)
+    next_btn = tk.Button(root, text='Next', command=lambda: page_changer([input_box.get()]), padx=30, pady=5)
 
     title_label.pack()
     desc_msg.pack()
+    input_box.pack()
     next_btn.pack()
 
 def results_page(root):
     title_label = tk.Label(root, text='Results')
-
     title_label.pack()
 
+    restaurants = get_restaurants()
+    for r in restaurants:
+        tk.Label(root, text=r['name']).pack()
+
+def get_new_restaurant():
+    global root
+
+    clear_window(root)
+    rand_restaurant = restaurants[0]
+    results_page(root)
+
 # Handles finding input errors, such as leaving required inputs blank
-def handle_errors():
+def handle_errors(values):
     global curr_page, root, error
 
     if curr_page == 1:
         # If the location was not specified on location page, do not let the user move onto the next page
-        if location == '':
+        if values[0].replace(' ', '') == '':
             # If first time seeing error, show the error message
             if error == False:
                 err_label = tk.Label(root, text='Location is mandatory')
@@ -76,15 +98,18 @@ def handle_errors():
     # No input error was found
     error = False
 
-def page_changer():
-    global curr_page, root, error
+def clear_window(root):
+    for widget in root.winfo_children():
+        widget.destroy()
 
-    handle_errors()
+def page_changer(values):
+    global curr_page, root, error, location, radius, term
+
+    handle_errors(values)
 
     # Destroy all the current widgets on the root (current page)
     if error == False:
-        for widget in root.winfo_children():
-            widget.destroy()
+        clear_window(root)
 
     # Put the widgets for the new page onto the root
     if curr_page == 0:
@@ -93,17 +118,24 @@ def page_changer():
     elif curr_page == 1:
         # If the location was not specified, do not let the user move onto the next page
         if error == False:
+            # Set the mandatory location and optional radius (convert mi to m)
+            location = values[0]
+            if values[1] != '':
+                radius = int(values[1]) * 1609
             choices_page(root)
             curr_page = 2
     elif curr_page == 2:
+        term = values[0]
         results_page(root)
 
 # Create the GUI root widget
 root = tk.Tk()
 root.title("Restaurant Picker")
-root.geometry('450x200')
+root.geometry('450x400')
+
 # Start the program with the main page
 main_page(root)
 curr_page = 0
 error = False
+
 root.mainloop()
